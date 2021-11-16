7b444cd012340b0a90ae394d32fd6ba39595d8d4
SapphicCynic <Sapphiccynic@gmail.com>
2021-11-16 23:05:24 +1300

    added intro screen and customer registration function

--- NZ Taxi Trip Booking System.cpp
+++ NZ Taxi Trip Booking System.cpp
@@ -31,6 +31,7 @@ int noOfNewCustomersToday = 0;
 int* noOfNewCustomersTodayPtr = &noOfNewCustomersToday;
 int noOfNewCustomersThisWeek = 0;
 int* noOfNewCustomersThisWeekPtr = &noOfNewCustomersThisWeek;
+fstream customerData;
 
 // Global Variables For Customer Screen:
 double costPerKm = 2.60;
@@ -156,6 +157,21 @@ struct Driver
     // Endorsement Information:
     string endorsementNumber = "P";
     int endorsementExpiry[3] = { 0,0,0 };
+};
+
+
+struct Customer {
+    // General Information:
+    string fullName;
+    string contactNumber;
+    string postalAddress;
+    string paymentMethod;
+
+    // Account LogIn Information
+    string emailAddress;
+    string password1;
+    string password2;
+    string passwordConfirmed;
 };
 
 // Driver Activity Struct / Claimed Trips:
@@ -658,7 +674,31 @@ void cleanUpTrips()
 void introFunction()
 {
     // Placeholder for the intro screen:
-    cout << "\n\n\t\t\t[intro screen]" << endl << endl;
+    cout << endl << endl;
+    cout << "\t                  ---" << endl;
+    cout << "\t                  | |" << endl;
+    cout << "\t                 ....." << endl;
+    cout << "\t           , ,''  |    ```...___," << endl;
+    cout << "\t    .--  ''  P(___|_______/    (|" << endl;
+    cout << "\t  ( //            |             |" << endl;
+    cout << "\t  ` ._: ' ' :_____|______: ' ' :/" << endl;
+    cout << "\t      '  o  '            '  o  '" << endl;
+    cout << "\t        - -                - - " << endl;
+    cout << "\t _____________________________________________" << endl;
+    cout << "\t _________Welcome to NZ Taxi Trip~!___________" << endl;
+    cout << "\t _____________________________________________" << endl << endl;
+    cout << "\t ________________T&C's Apply__________________" << endl << endl;
+    cout << "\t ________Customer Terms & Conditions__________" << endl;
+    cout << "1. Wear a face mask during your ride(can be temporarily removed for the purpose of drinking water from a bottle) \n unless you have a medical exemption(in which case you will need to provide proof of this)." << endl;
+    cout << "2. Do not ride with us if you are awaiting the results of a Covid test, suspect you may have Covid, \n or are currently showing cold or flu - like symptoms." << endl;
+    cout << "3. There is to be no consumption of hot food in the cab, nor any drinks that may spill." << endl;
+
+    cout << " __________Driver Terms& Conditions___________" << endl;
+    cout << "1. In order to drive for us, you will need to have had both Covid - 19 vaccinations." << endl;
+    cout << "2. Always wear a face mask when driving with a passenger(unless you have a medical exemption)." << endl;
+    cout << "3. You must be at least 22 years of age to drive for us." << endl;
+    cout << "4. Always conduct yourself in a polite and professional manner with customers." << endl;
+    cout << "5. All drivers who choose to participate in NZ Taxi Trip do so at their own discretion,\n as an independent contractor." << endl;
 
     // Use a function to determine the user's role:
     roleChoiceFunction();
@@ -770,7 +810,68 @@ void customerLogIn()
 // Customer Registration Function:
 void customerRegistration()
 {
-    cout << "\n\n\n\t\t\t[Insert Customer Registration Here]" << endl;
+    Customer newCustomer;
+    // Registration Section:
+    cout << "\n\n\n\t\t___________________________________________________" << endl << endl;
+    cout << "\t\t---------------------------------------------------" << endl;
+    cout << "\t\t___________________________________________________" << endl << endl;
+
+    cout << "\n\t\t\tRegistration" << endl << endl << endl;
+
+    // Fix the spaces causing missed inputs issue:
+    cin.ignore();
+
+    // Gather Input:
+    cout << "\t\t\t|| General Information ||" << endl << endl;
+    cout << "\t\tFull Name: ";
+    getline(cin, newCustomer.fullName);
+    cout << "\t\tContact Number: ";
+    getline(cin, newCustomer.contactNumber);
+    cout << "\t\tPostal Address: ";
+    getline(cin, newCustomer.postalAddress);
+
+    cout << "\n\n\n\t\t\t|| Payment Information ||" << endl << endl;
+    cout << "\t\tPayment Method: ";
+    getline(cin, newCustomer.paymentMethod);
+
+    cout << "\n\n\n\t\t\t|| Account LogIn Information ||" << endl << endl;
+    cout << "\t\tEmail Address: ";
+    getline(cin, newCustomer.emailAddress);
+    cout << "\t\tCreate a Password: ";
+    getline(cin, newCustomer.password1);
+    cout << "\t\tRe-enter Your Password: ";
+    getline(cin, newCustomer.password2);
+
+    if (newCustomer.password1 == newCustomer.password2)
+    {
+        newCustomer.passwordConfirmed = newCustomer.password1;
+    }
+    else
+    {
+        while (newCustomer.password1 != newCustomer.password2)
+        {
+            cout << "\n\t\tError. The passwords you have entered do not match." << endl;
+            cout << "\t\tPlease try again." << endl << endl << endl;
+
+            cout << "\t\tCreate a Password: ";
+            getline(cin, newCustomer.password1);
+            cout << "\t\tRe-enter Your Password: ";
+            getline(cin, newCustomer.password2);
+        }
+        // Once they do match, confirm:
+        newCustomer.passwordConfirmed = newCustomer.password1;
+    }
+    // Enter details into the customerData.txt file:
+
+    customerData.open("customerData.txt", ios::out | ios::app);
+    customerData << newCustomer.fullName << endl;
+    customerData << newCustomer.contactNumber << endl;
+    customerData << newCustomer.postalAddress << endl;
+    customerData << newCustomer.paymentMethod << endl;
+    customerData << newCustomer.emailAddress << endl;
+    customerData << newCustomer.passwordConfirmed << endl;
+    customerData << "-----End of item-----" << endl;
+    customerData.close();
 }
 
 // Customer Screen:
